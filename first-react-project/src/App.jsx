af1480916045b12b2ce05d5ae74224ebfc9a8b79
YousefKurchaloy <yousef.shaher@gmail.com>
2024-02-22 22:50:48 +0300

    first commit

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Student from "./Student";
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="font-blue">Hello world</h1>
+      <Student />
+    </div>
+  );
+};
+export default App;
---
+++ first-react-project/src/Student.jsx
@@ -0,0 +1,15 @@
+const Student = () => {
+  return (
+    <div>
+      <h2>Student</h2>
+      <ul>
+        <li>Name: Yousef</li>
+        <li>Id: 20223</li>
+        <li>Status: Didnt graduate yet</li>
+        <li>Date of Birth: 2005/6/5</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Student;
